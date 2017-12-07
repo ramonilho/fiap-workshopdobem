b9e7c47294a50c427ff0c384b00fd4a4f40a3d09
Ramon Honorio <ramonaqh@gmail.com>
2017-12-06 22:34:14 -0200

    Add project files.

---
+++ API.WorkshopDoBem/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(API.WorkshopDoBem.Startup))]
+
+namespace API.WorkshopDoBem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
